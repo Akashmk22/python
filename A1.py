3dfa28256c121fb877dfe17897d25afb0d86b23b
Akash MK <97655039+Akashmk22@users.noreply.github.com>
2023-04-03 14:53:49 +0530

    Update A1.py

--- A1.py
+++ A1.py
@@ -1,11 +1,19 @@
-from zipfile import ZipFile
+import zipfile
+import os
 
-z=ZipFile("sample.zip",'w')
+def zip_folder(folder_path, output_path):
+    # Get the directory name and the name for the output zip file
+    directory_name = os.path.basename(folder_path)
+    zip_file_name = directory_name + '.zip'
 
-z.write("A2.py")
-z.write("A3.py")
-z.write("A1.txt")
-z.write("A2.txt")
+    # Create a ZipFile object with the output path and the zip file name
+    with zipfile.ZipFile(output_path + '\\' + zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        # Walk through the directory and add all files and directories to the zip file
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                zipf.write(os.path.join(root, file))
 
-
-z.close()
+if __name__ == '__main__':
+    folder_path = 'output'
+    output_path = 'C:/Users/Akash M K/Desktop/output22' # Change this to the directory where you want to save the zip file
+    zip_folder(folder_path, output_path)
